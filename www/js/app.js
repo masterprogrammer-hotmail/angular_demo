4907dccccc557cb3bcfddcb3d466ab1091f9460e
Jim Walker <jim.walkster@gmail.com>
2018-01-28 13:51:29 -0800

    Added listgroup clicks

--- www/js/app.js
+++ www/js/app.js
@@ -6,7 +6,17 @@ angular.module('Demo', [])
   .controller('demoCtrl', ['$scope', function ($scope) {
     $scope.greetingEN = 'Hello';
     $scope.greetingSP = 'Hola';
+    $scope.greeting = '';
     console.log('app.js');
+
+    $scope.setEnglish = function() {
+      $scope.greeting = $scope.greetingEN;
+    };
+
+    $scope.setSpanish = function() {
+      $scope.greeting = $scope.greetingSP;
+    };
+
 }]);
 
 });
