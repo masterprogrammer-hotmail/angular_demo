d23b5bb3323012cf6952c04ff386ebcee5f67623
mark stutts <masterprogrammer@hotmail.com>
2018-01-28 17:14:55 -0700

    fix whitespace

--- www/controllers/democtrl.js
+++ www/controllers/democtrl.js
@@ -1,20 +1,19 @@
 define(['angular', '../services/greeting'], function () {
 
+  angular.module('app')
+    .controller('demoCtrl', ['$scope','GreetingSVC', function ($scope, GreetingSVC) {
+    //.controller('demoCtrl', ['$scope', function ($scope) {
 
-angular.module('app')
-  .controller('demoCtrl', ['$scope','GreetingSVC', function ($scope, GreetingSVC) {
-  //.controller('demoCtrl', ['$scope', function ($scope) {
-
-    $scope.greeting = GreetingSVC.en;
-    //$scope.greeting = "";
-
-    $scope.setEnglish = function() {
       $scope.greeting = GreetingSVC.en;
-    };
+      //$scope.greeting = "";
 
-    $scope.setSpanish = function() {
-      $scope.greeting = GreetingSVC.sp;
-    };
-}]);
+      $scope.setEnglish = function() {
+        $scope.greeting = GreetingSVC.en;
+      };
+
+      $scope.setSpanish = function() {
+        $scope.greeting = GreetingSVC.sp;
+      };
+  }]);
 
 });
