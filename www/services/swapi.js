c84b5b261381a38b365d1f1c3e1d0a1d72ed4e4f
Jim Walker <jim.walkster@gmail.com>
2018-02-01 13:50:04 -0800

    swapi change

--- www/services/swapi.js
+++ www/services/swapi.js
@@ -1,11 +1,27 @@
 define(['angular'], function() {
 
     angular.module('app')
-      .factory('SWAPISVC', function($http) {
-          
-        var promise = $http.get('https://swapi.co/api/');
+      .factory('SWAPISVC', function($http, $q) {
+        var deferred = $q.defer();
 
-        return promise;
-    });
+        return $q(function(resolve, reject) {
+
+            $http.get('https://swapi.co/api/')
+                .then(function(response) {
+
+                    console.log("SWAPI SUCCESS: ", response);
+    
+                    resolve(response.data);
+            
+                }, function(response) {
+                    // On Error
+                    console.log('Error: ', response);
+
+                    reject('There was an error with the service call.');
+                });
+
+        }); // /$q
+
+    }); // /module
   
   });
